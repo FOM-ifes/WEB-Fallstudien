a8efcccb8fb6d9c8245dcf33b7430b1a2d14f202
Sebastian Sauer <ssauer@posteo.de>
2019-08-04 17:48:29 +0200

    update create-thumbnail.r

--- code-div/create-thumbnail-png.R
+++ code-div/create-thumbnail-png.R
@@ -94,6 +94,7 @@ create_imgfiles(title_casestudy = title_casestudy,
 
 # Read all case studies (titles)
 
+library(tidyverse)
 
 casestudies <- read_csv(paste0(here::here(), "/static/casestudies-overview.csv"))
 
@@ -104,12 +105,27 @@ casestudies
 
 k <- 30
 
+
+# test it:
 casestudies$description[1] %>% str_wrap(width = k) %>% paste0(., "\n") %>% str()
 
-casestudies <-
-  casestudies %>% 
-  mutate(description_wrapped = map(casestudies$description, ~ (str_wrap(., width = k) %>% paste0(., "\n")))) %>% 
-  unnest(description_wrapped)
+
+wrap_casestudies <- function(title, description, k = 30) {
+
+  casestudies_wrapped <-
+    casestudy_df %>% 
+    mutate(description_wrapped = map(casestudy_df$description, ~ (str_wrap(., width = k) %>% paste0(., "\n")))) %>% 
+    unnest(description_wrapped)
+  
+  casestudies_wrapped <-
+    casestudy_df %>% 
+    mutate(title_wrapped = map(casestudy_df$title, ~ (str_wrap(., width = k) %>% paste0(., "\n")))) %>% 
+    unnest(title_wrapped)
+  
+  return(casestudies_wrapped)
+}
+
+out <- wrap_casestudies(casestudies)
 
 
 
@@ -117,10 +133,14 @@ casestudies <-
 
 # Create images -------------------------------------------------
 
-
+# Create ALL images
 casestudies %>% 
-  pmap( ~ create_imgfiles(title_casestudy = ..5,
+  pmap( ~ create_imgfiles(title_casestudy = ..2,
                         title_short = ..1,
                         tags = ..4))
 
 
+# Create images for one casestudy (full images and thumb image)
+
+
+create_imgfiles(title_casestudy = "")
