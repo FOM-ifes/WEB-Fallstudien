fb01a76339cecc4f1b143975059c75847754ab37
Sebastian Sauer <ssauer@posteo.de>
2020-10-31 07:57:15 +0100

    fix typo

--- code-div/create-icon-for-casestudy.R
+++ code-div/create-icon-for-casestudy.R
@@ -6,7 +6,7 @@
 source("code-div/create-thumbnail-png-funs.R")
 
 
-create_imgfiles(title_casestudy = "Prädiktoren der Verspätungen der New Yorker Flüge", 
+create_imgfiles(title_casestudy = "Prädiktoren der Verspätungen\nder New Yorker Flüge", 
                 title_short = "NYC Flights",
                 tags = "Regression, Prognose")
 
